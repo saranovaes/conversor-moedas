21f2e165d977e609ca53a8cc94b97f06779df431
Sara Novaes <sarinhanovaes@hotmail.com>
2024-04-24 22:27:27 -0300

    Primeiro commit criando package Conversao.

---
+++ src/br/com/conversormoedas/Conversao.java
@@ -0,0 +1,30 @@
+package br.com.conversormoedas;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+import static br.com.conversormoedas.FormatarMoedas.formatarMoedas;
+
+public class Conversao {
+    // Recebe a moeda Original, a moeda conversora e a quantia original
+    public static String converter(String quantiaOriginal, Object moedaOriginal, Object moedaConversora)
+            throws IOException {
+
+        // Divide a quantia original, pelo valor da moedaConversora vinda da API, resultando no valorConvertido.
+        double valorConvertido = Double.valueOf(quantiaOriginal) / Double.valueOf(getValorMoedaConversoraAPI(moedaOriginal, moedaConversora));
+
+        return new DecimalFormat("#.###").format(valorConvertido);
+    }
+
+    // Define o tipo de moeda e faz requisição para a API.
+    public static String getValorMoedaConversoraAPI(Object moedaOriginal, Object moedaConversora) {
+        String moedaOriginalSTRING = formatarMoedas(moedaOriginal);
+        String moedaConversoraSTRING = formatarMoedas(moedaConversora);
+
+        // Recebe as moedas, ex: USD-BRL e faz requisição para a moneyAPI
+        String valorMoedaConversoraAPI = MoneyAPI.sendRequestAPI(moedaOriginalSTRING, moedaConversoraSTRING);
+
+        return valorMoedaConversoraAPI.toString();
+    }
+}
+
